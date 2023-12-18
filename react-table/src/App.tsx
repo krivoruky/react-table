7291c2a03839f6dff611771d90ab22755b31ddb0
Станислав Тихомиров <tsv@digt.ru>
2023-12-18 17:08:22 +0300

    init: start project

---
+++ react-table/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <header>
+      <select name="select">
+        <option value="value1"selected>select api</option>
+        <option value="value2">Значение 2</option>
+        <option value="value3">Значение 3</option>
+      </select>
+      </header>
+      <main>
+        <section>
+          <div>
+            wefwefwef
+          </div>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
